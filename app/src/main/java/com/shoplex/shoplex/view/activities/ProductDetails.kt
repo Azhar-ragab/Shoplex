6d97eadc9785412157ad164757060f0c4afd954f
Heba-ali95 <hebaaliali18@gmail.com>
2021-05-16 21:36:31 +0200

    Navigation-Button

--- app/src/main/java/com/shoplex/shoplex/view/activities/ProductDetails.kt
+++ app/src/main/java/com/shoplex/shoplex/view/activities/ProductDetails.kt
@@ -1,20 +1,22 @@
-package com.shoplex.shoplex
+package com.shoplex.shoplex.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 
 import com.google.android.material.tabs.TabLayout
+import com.shoplex.shoplex.PagerAdapter
+import com.shoplex.shoplex.R
 import com.shoplex.shoplex.databinding.ActivityProductDetailsBinding
 
 class ProductDetails : AppCompatActivity() {
     private lateinit var binding: ActivityProductDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_product_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product_details)
 
 
-        var pagerAdapter: PagerAdapter =PagerAdapter(supportFragmentManager)
+        var pagerAdapter: PagerAdapter = PagerAdapter(supportFragmentManager)
         binding.tabLayout.setupWithViewPager(binding.viewPager)
         binding.viewPager.adapter = pagerAdapter
         binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
