7b28f6a77b7469919a3765fd75751d5a221b1674
Heba-ali95 <hebaaliali18@gmail.com>
2021-05-23 02:34:01 +0200

    signupActivity

--- app/src/main/java/com/shoplex/shoplex/view/activities/SignupActivity.kt
+++ app/src/main/java/com/shoplex/shoplex/view/activities/SignupActivity.kt
@@ -4,46 +4,77 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.shoplex.shoplex.R
 import com.shoplex.shoplex.databinding.ActivitySignupBinding
+import com.shoplex.shoplex.model.extra.FirebaseReferences
 import com.shoplex.shoplex.model.pojo.User
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 class SignupActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
-    private val database = Firebase.firestore
-    private var client: User = User()
-    private lateinit var pendingSellerRef: CollectionReference
-    private val MAPS_CODE = 202
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
         binding.btnSignup.setOnClickListener {
-            client.image =
+            val name = binding.edName.text.toString()
+            val email = binding.edEmail.text.toString()
+            val phone = binding.edPassword.text.toString()
+            val img =
                 "https://img.etimg.com/thumb/width-1200,height-900,imgsize-122620,resizemode-1,msid-75214721/industry/services/retail/future-group-negotiates-rents-for-its-1700-stores.jpg"
-            client.name = binding.edName.text.toString()
-            client.email = binding.edEmail.text.toString()
-            client.phone = binding.edPhone.text.toString()
-            client.date = Timestamp.now().toDate()
-
+            val user =
+                User("", name, email, arrayListOf(), phone, img, null, arrayListOf(), arrayListOf())
+            addUser(user)
             if (checkEditText() == true) {
-                addSeller(closeContextMenu())
-                startActivity(Intent(this, HomeActivity::class.java))
+                addUser(user)
+                startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }
+        createAccount(email,binding.edPassword.text.toString())
         }
         binding.btnLocation.setOnClickListener {
-            startActivityForResult(Intent(this, HomeActivity::class.java), MAPS_CODE)
         }
+
+    }
+
+    fun addUser(user: User) {
+        FirebaseReferences.userRef.add(user).addOnSuccessListener {
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    private fun createAccount(email: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "createUserWithEmail:success")
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        // [END create_user_with_email]
     }
     //check EditText
     fun checkEditText(): Boolean {
@@ -70,8 +101,6 @@ class SignupActivity : AppCompatActivity() {
                 )
 
             binding.edPhone.length() == 0 -> binding.edPhone.error = getString(R.string.Required)
-            client.addresses.size == 0 || client.locations?.size == 0 -> Toast.makeText(this,"Choose Your Location",
-                Toast.LENGTH_LONG).show()
             else -> return true
         }
         return false
@@ -82,19 +111,5 @@ class SignupActivity : AppCompatActivity() {
         val pattern: Pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
         val matcher: Matcher = pattern.matcher(email)
         return matcher.matches()
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == MAPS_CODE){
-            if(resultCode == RESULT_OK){
-                val location: Parcelable? = data?.getParcelableExtra("Loc")
-                if(location != null) {
-                    val address = getAddress(location as LatLng,this)
-                    client.locations.add(location)
-                    binding.tvLocation.text = address
-                    client.addresses.add(address)
-                }
-            }
-        }
     }
 }
