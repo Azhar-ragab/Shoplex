88a02fc3fa84cccbb9a65b3e4c6eec5028b3dc16
Heba-ali95 <hebaaliali18@gmail.com>
2021-05-25 19:57:50 +0200

    Descriotion Video

--- app/src/main/java/com/shoplex/shoplex/view/activities/DescriptionActivity.kt
+++ app/src/main/java/com/shoplex/shoplex/view/activities/DescriptionActivity.kt
@@ -1,49 +1,50 @@
 package com.shoplex.shoplex.view.activities
 
 import android.content.Intent
+import android.media.MediaPlayer.OnCompletionListener
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.widget.MediaController
+import androidx.appcompat.app.AppCompatActivity
 import com.shoplex.shoplex.R
 import com.shoplex.shoplex.databinding.ActivityDescriptionBinding
-import com.shoplex.shoplex.databinding.ActivityHomeBinding
+
 
 class DescriptionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDescriptionBinding
-    private var position =0
-    private var mediaControls :MediaController ?= null
+    private var position = 0
+    private var mediaControls: MediaController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDescriptionBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnEnglish?.setOnClickListener(){
+        binding.btnEnglish?.setOnClickListener() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
-        binding.btnArabic?.setOnClickListener(){
+        binding.btnArabic?.setOnClickListener() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
-        if (mediaControls == null){
-            mediaControls = MediaController(this@DescriptionActivity)
-        }
+        binding.vvVideoView.setOnCompletionListener(OnCompletionListener {
+            binding.vvVideoView.start()
+        })
+
         try {
-          binding. vvVideoView.setMediaController(mediaControls)
-            binding.vvVideoView.setVideoURI(Uri.parse("android.resource://"+packageName+"/"+R.raw.videodesc))
-        }catch (e:Exception){
+            binding.vvVideoView.setMediaController(mediaControls)
+            binding.vvVideoView.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.onlineshopping))
+        } catch (e: Exception) {
             e.message?.let { Log.e("Error", it) }
         }
         binding.vvVideoView.requestFocus()
-        binding.vvVideoView.setOnPreparedListener{
+        binding.vvVideoView.setOnPreparedListener {
             binding.vvVideoView.seekTo(position)
-            if (position==0){
+            if (position == 0) {
                 binding.vvVideoView.start()
-            }else{
+            } else {
                 binding.vvVideoView.pause()
             }
         }
@@ -52,8 +53,8 @@ class DescriptionActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        if (outState!=null){
-            outState.putInt("Position",binding.vvVideoView.currentPosition)
+        if (outState != null) {
+            outState.putInt("Position", binding.vvVideoView.currentPosition)
         }
         binding.vvVideoView.pause()
     }
