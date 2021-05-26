a3a4e1ad135235f8deaadb03ccf221d20718616f
habiba811 <habiba.hafez256@gmail.com>
2021-05-26 19:42:20 +0200

    Commit

--- app/src/main/java/com/shoplex/shoplex/model/pojo/User.kt
+++ app/src/main/java/com/shoplex/shoplex/model/pojo/User.kt
@@ -1,13 +1,15 @@
 package com.shoplex.shoplex.model.pojo
 
+import com.google.android.gms.maps.model.LatLng
+
 data class User(
     val userID: String = "",
     val name: String = "",
     val email: String = "",
-    val location: Location = Location(0.0, 0.0),
+    val location: Location =Location(0.0,0.0),
     var address: String = "",
     val phone: String = "",
-    val image: String = "",
+    val image: String? = "",
     val favouriteList: ArrayList<String> = ArrayList(),
     val cartList: ArrayList<String> = ArrayList()
 ) {
--- app/src/main/java/com/shoplex/shoplex/view/activities/SignupActivity.kt
+++ app/src/main/java/com/shoplex/shoplex/view/activities/SignupActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.ktx.Firebase
@@ -30,7 +29,7 @@ class SignupActivity : AppCompatActivity() {
         binding.btnSignup.setOnClickListener {
             val name = binding.edName.text.toString()
             val email = binding.edEmail.text.toString()
-            val phone = binding.edPassword.text.toString()
+            val phone = binding.edPhone.text.toString()
             val img =
                 "https://img.etimg.com/thumb/width-1200,height-900,imgsize-122620,resizemode-1,msid-75214721/industry/services/retail/future-group-negotiates-rents-for-its-1700-stores.jpg"
 
