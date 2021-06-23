387630f9952206f993ab299d70157896c6838fdd
Mohamed Elsayed <mohamed_elsayed.1995@yahoo.com>
2021-06-23 22:06:07 +0200

    Update Some Features

--- app/src/main/java/com/shoplex/shoplex/view/activities/FilterActivity.kt
+++ app/src/main/java/com/shoplex/shoplex/view/activities/FilterActivity.kt
@@ -96,9 +96,10 @@ class FilterActivity : AppCompatActivity() {
         binding.rsPrice.setLabelFormatter { value: Float ->
             val format = NumberFormat.getCurrencyInstance()
             format.maximumFractionDigits = 0
-            format.currency = Currency.getInstance(getString(R.string.EGP))
+            format.currency = Currency.getInstance(getString(R.string.StrEGP))
             format.format(value.toDouble())
         }
+
 
         binding.cardCategoryFilter.setOnClickListener {
             subCategoryBottomSheetDialog()
